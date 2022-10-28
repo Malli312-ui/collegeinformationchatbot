430d744d7834ca8a53a1da6a390b187aa79ef835
Malli312-ui <115055178+Malli312-ui@users.noreply.github.com>
2022-10-28 11:27:42 +0530

    Update speech.js

--- speech.js
+++ speech.js
@@ -6,7 +6,7 @@ const textToSpeech = (string) => {
   let voice = new SpeechSynthesisUtterance(string);
   voice.text = string;
   voice.lang = "en-US";
-  voice.volume = 0.5;
+  voice.volume = 100;
   voice.rate = 0.7;
   voice.pitch = 0.5; // Can be 0, 1, or 2
   synth.speak(voice);
